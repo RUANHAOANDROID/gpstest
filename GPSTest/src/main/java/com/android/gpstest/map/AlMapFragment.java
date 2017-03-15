24065e28296b209781ddf6ece7e631af26268f56
rh <52927295@qq.com>
2017-03-15 10:55:42 +0800

    test commit

---
+++ GPSTest/src/main/java/com/android/gpstest/map/AlMapFragment.java
@@ -0,0 +1,15 @@
+package com.android.gpstest.map;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by rh on 2016/12/20.
+ */
+
+public class AlMapFragment extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
